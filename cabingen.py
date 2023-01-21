dc2b533c11a158ef3bcf5e09db8876fa605d8e6a
LDaxin <46381046+LDaxin@users.noreply.github.com>
2023-01-22 00:33:19 +0100

    addet the option to make a shelf out of a Cabinet

--- cabingen.py
+++ cabingen.py
@@ -4,61 +4,72 @@ import os
 #changebal Vars
 WoodThiknes = 1.9
 
-CabinetfWidth = 50
-CabinetfHeight = 70
-CabinetfDeeps = 34
+CabinetWidth = 50
+CabinetHeight = 70
+CabinetDeeps = 34
 
 Abstand = 0.4
 
 BordAmount = 3 
 
-TopBord = False
+TopBord = True
+Door = False
 
 #Calculated Vars
 
-DoorWidth = CabinetfWidth - (Abstand*2)
-DoorHeight = CabinetfHeight - (Abstand*2)
+DoorWidth = CabinetWidth - (Abstand*2)
+DoorHeight = CabinetHeight - (Abstand*2)
 
 if TopBord:
     heightMod = WoodThiknes
 else:
     heightMod = 0
 
-SideWidth = CabinetfDeeps-Abstand-WoodThiknes 
-SideHeight = CabinetfHeight - WoodThiknes - heightMod
+SideHeight = CabinetHeight - WoodThiknes - heightMod
 
-BackWidth = CabinetfWidth-(WoodThiknes*2)
-BackHeight = CabinetfHeight - WoodThiknes - heightMod 
+BackWidth = CabinetWidth-(WoodThiknes*2)
+BackHeight = CabinetHeight - WoodThiknes - heightMod 
 
-BordWidth = CabinetfWidth-(WoodThiknes*2)
-BordHeight = CabinetfDeeps-Abstand-WoodThiknes
+BordWidth = CabinetWidth-(WoodThiknes*2)
 
-TopBordWidth = CabinetfWidth
-TopBordHeight = CabinetfDeeps-WoodThiknes-Abstand
+TopBordWidth = CabinetWidth
+
+if Door:
+    DoorDistance = Abstand + WoodThiknes
+    BordHeight = CabinetDeeps-Abstand-(WoodThiknes*2)
+    TopBordHeight = CabinetDeeps-WoodThiknes-Abstand
+    SideWidth = CabinetDeeps-Abstand-WoodThiknes 
+
+else:
+    DoorDistance = 0
+    BordHeight = CabinetDeeps-Abstand-WoodThiknes
+    TopBordHeight = CabinetDeeps
+    SideWidth = CabinetDeeps
 
 
 # XYZ
 cabinet = cadquery.Assembly()
 door = cadquery.Workplane().box(DoorWidth, WoodThiknes, DoorHeight, False).translate([Abstand, 0, Abstand])
 
-left = cadquery.Workplane().box(WoodThiknes, SideWidth, SideHeight, False).translate([0, (Abstand + WoodThiknes), WoodThiknes])
+left = cadquery.Workplane().box(WoodThiknes, SideWidth, SideHeight, False).translate([0, DoorDistance, WoodThiknes])
 
-right = cadquery.Workplane().box(WoodThiknes, SideWidth, SideHeight, False).translate([CabinetfWidth-WoodThiknes, Abstand+WoodThiknes, WoodThiknes])
+right = cadquery.Workplane().box(WoodThiknes, SideWidth, SideHeight, False).translate([CabinetWidth-WoodThiknes, DoorDistance, WoodThiknes])
 
-back = cadquery.Workplane().box(BackWidth, WoodThiknes, BackHeight, False).translate([WoodThiknes, CabinetfDeeps - WoodThiknes, WoodThiknes])
+back = cadquery.Workplane().box(BackWidth, WoodThiknes, BackHeight, False).translate([WoodThiknes, CabinetDeeps - WoodThiknes, WoodThiknes])
 
-bord = cadquery.Workplane().box(TopBordWidth,TopBordHeight, WoodThiknes, False).translate([ 0, Abstand+WoodThiknes, 0])
+bord = cadquery.Workplane().box(TopBordWidth,TopBordHeight, WoodThiknes, False).translate([ 0, DoorDistance, 0])
 cabinet.add(bord)
 
 for x in range(0, BordAmount +1):
-    bord = cadquery.Workplane().box(BordWidth,BordHeight, WoodThiknes, False).translate([ WoodThiknes, Abstand+WoodThiknes, (CabinetfHeight/(BordAmount+1))*x])
+    bord = cadquery.Workplane().box(BordWidth,BordHeight, WoodThiknes, False).translate([ WoodThiknes, DoorDistance, (CabinetHeight/(BordAmount+1))*x])
     cabinet.add(bord)
 
 if TopBord:
-    bord = cadquery.Workplane().box(TopBordWidth,TopBordHeight, WoodThiknes, False).translate([ 0, Abstand+WoodThiknes, CabinetfHeight-WoodThiknes])
+    bord = cadquery.Workplane().box(TopBordWidth,TopBordHeight, WoodThiknes, False).translate([ 0, DoorDistance, CabinetHeight-WoodThiknes])
     cabinet.add(bord)
 
-cabinet.add(door)
+if Door:
+    cabinet.add(door)
 cabinet.add(left)
 cabinet.add(right)
 cabinet.add(back)
@@ -70,7 +81,7 @@ if not os.path.exists('./saves'):
 
 cadquery.exporters.export(stl, './saves/view.stl')
 
-name = 'cabinet_'+ str(CabinetfWidth*10) + 'mmX' + str(CabinetfDeeps*10) + 'mmX' + str(CabinetfHeight*10) + '_bord_tikness_'+ str(WoodThiknes*10) +'mm'
+name = 'cabinet_'+ str(CabinetWidth*10) + 'mmX' + str(CabinetDeeps*10) + 'mmX' + str(CabinetHeight*10) + '_bord_tikness_'+ str(WoodThiknes*10) +'mm'
 
 with open('./saves/'+ name + '.txt', 'w') as f:
     f.writelines('Door: '+ str(DoorWidth*10) + 'mm X ' + str(DoorHeight*10) + 'mm X' + str(WoodThiknes*10) + 'mm\n')
